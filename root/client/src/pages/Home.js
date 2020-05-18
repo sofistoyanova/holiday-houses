413f666e7ba58f713271a4b7dc2f3a4dcb16ee1b
Larisa <ailisoaielarisa@yahoo.com>
2020-05-18 23:15:16 +0200

    working on home pagee, add bookings route

--- root/client/src/pages/Home.js
+++ root/client/src/pages/Home.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import MainFilter from '../components/top/MainFilter.js'
 import HeroImage from '../components/top/HeroImage.js'
 import HomeSpaces from '../components/HomeSpaces.js'
 import HomePerfectPlace from '../components/HomePerfectPlace.js'
+import Experiences from '../components/Experiences.js'
 import WhyUs from '../components/WhyUs.js'
+import MiddleBanner from '../components/MiddleBanner.js'
+import Footer from '../components/Footer.js'
 
 // import {Link, useHistory, useLocation } from "react-router-dom"
 
@@ -16,8 +19,9 @@ const Home = (props) => {
         <HomeSpaces></HomeSpaces>
         <HomePerfectPlace></HomePerfectPlace>
         <WhyUs></WhyUs>
-		
-      
+        <MiddleBanner></MiddleBanner>
+        <Experiences></Experiences>
+        <Footer></Footer>
 
     </div>
   )
