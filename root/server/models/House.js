cefd28d452040909e8cab5213d3dbc26fcf0b427
Sofi <sofi.stoyanova2@gmail.com>
2020-05-19 20:12:36 +0200

    add bookings routes

--- root/server/models/House.js
+++ root/server/models/House.js
@@ -4,6 +4,12 @@ class House extends Model {
     static get tableName() {
         return 'houses'
     }
+
+      // return single record or null
+    static async singleOrDefault (query) {
+        let result = await this.query().select().where(query).limit(1);
+        return result[0];
+    }
 }
 
 module.exports = House
