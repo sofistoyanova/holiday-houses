147b3654c317102e205f329e8c2abdc165162b4d
Larisa <ailisoaielarisa@yahoo.com>
2020-05-25 20:40:23 +0200

    work in progress

--- root/client/src/components/top/Nav.js
+++ root/client/src/components/top/Nav.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from '../../pages/Login.js'
 import Signup from '../../pages/Signup.js'
 import Home from '../../pages/Home.js'
-import Bookings from '../../pages/Bookings.js'
+import SeeAll from '../../pages/SeeAll.js'
 
 const Nav = () => {
 
@@ -15,7 +15,7 @@ const Nav = () => {
         <ul className="d-flex justify-content-end">
           <h5 className="m-2"><Link to='/'>Home</Link></h5>
           <br></br>
-          <h5 className="m-2"><Link to='/bookings'>Bookings</Link></h5>
+          <h5 className="m-2"><Link to='/seeAll'>See all</Link></h5>
           <br></br>
           <h5 className="m-2"> <Link to='/login'>Login</Link></h5>
           <br></br>
@@ -34,8 +34,8 @@ const Nav = () => {
                 <Route exact path="/">
                   <Home />
                 </Route>
-                <Route exact path="/bookings">
-                  <Bookings />
+                <Route exact path="/seeAll">
+                  <SeeAll />
                 </Route>
             </Switch>
         </div>
